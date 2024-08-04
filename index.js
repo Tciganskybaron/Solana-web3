1669d59dd8ce98492f3b71d0337901d954264074
Tciganskybaron <Tihonov.n.01@gmail.com>
2024-08-04 22:21:10 +0200

    add mintSPL

--- index.js
+++ index.js
@@ -4,17 +4,43 @@ const solanaWeb3 = require('@solana/web3.js');
 const splToken = require('@solana/spl-token');
 const bs58 = require('bs58').default; 
 
-async function main() {
-	const connection = new solanaWeb3.Connection('https://solana-mainnet.core.chainstack.com/b490b7141d006a5b9f894cdc113e6150', {
-		wsEndpoint: 'wss://solana-mainnet.core.chainstack.com/b490b7141d006a5b9f894cdc113e6150',
-	});
+const connection = new solanaWeb3.Connection('https://solana-mainnet.core.chainstack.com/b490b7141d006a5b9f894cdc113e6150', {
+	wsEndpoint: 'wss://solana-mainnet.core.chainstack.com/b490b7141d006a5b9f894cdc113e6150',
+});
 
-	const walletKeyPair = solanaWeb3.Keypair.fromSecretKey(new Uint8Array(bs58.decode(process.env.PRIVATE_KEY)));
+const walletKeyPair = solanaWeb3.Keypair.fromSecretKey(new Uint8Array(bs58.decode(process.env.PRIVATE_KEY)));
 
+async function getBalance () {
 	let balance = await connection.getBalance(walletKeyPair.publicKey);
 	console.log(balance / solanaWeb3.LAMPORTS_PER_SOL);
 }
 
-main().catch(err => {
-	console.error(err);
-});
+async function mintSPL() {
+
+	const mint = await splToken.createMint(
+		connection,
+		walletKeyPair,
+		walletKeyPair.publicKey,
+		null,
+		9, 
+		undefined,
+		{},
+		splToken.TOKEN_PROGRAM_ID
+	);
+
+	const tokenAccount = await splToken.getOrCreateAssociatedTokenAccount(connection, walletKeyPair, mint, walletKeyPair.publicKey);
+
+  await splToken.mintTo(
+    connection,
+    walletKeyPair,
+    mint,
+    tokenAccount.address,
+    walletKeyPair.publicKey,
+    1000000000000,
+  )
+
+	return mint;
+}
+
+
+const mintAddress = await mintSPL()
