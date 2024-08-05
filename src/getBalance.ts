aaa2a240ea06da8fdef59743f1cd80d3b0f2329f
Tciganskybaron <Tihonov.n.01@gmail.com>
2024-08-05 21:56:41 +0200

    getBalance test

---
+++ src/getBalance.ts
@@ -0,0 +1,9 @@
+import * as solanaWeb3 from '@solana/web3.js';
+
+// Функция для проверки баланса на кошельке
+export async function getBalance(connection: solanaWeb3.Connection, walletKeyPair: solanaWeb3.Keypair): Promise<number> {
+  // Получение баланса в лампортах (малая единица SOL)
+  const balance = await connection.getBalance(walletKeyPair.publicKey);
+  // Конвертация баланса в SOL и вывод в консоль
+  return balance / solanaWeb3.LAMPORTS_PER_SOL;
+}
---
+++ test/getBalance.test.ts
@@ -0,0 +1,40 @@
+import bs58 from 'bs58';
+import dotenv from 'dotenv';
+import { describe, test, expect, jest } from '@jest/globals';
+import { getBalance } from '../src/getBalance';
+import { Connection, Keypair } from '@solana/web3.js';
+import { HTTPS_ENDPOINT, PRIVATE_KEY } from '../src/config'
+
+dotenv.config();
+
+const mockConnection = new Connection(HTTPS_ENDPOINT);
+const mockKeypair = Keypair.fromSecretKey(new Uint8Array(bs58.decode(PRIVATE_KEY)));
+
+describe('getBalance function', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should connect to the Solana network', async () => {
+    const getBalanceSpy = jest.spyOn(mockConnection, 'getBalance');
+
+    const balance = await getBalance(mockConnection, mockKeypair);
+    
+    expect(getBalanceSpy).toHaveBeenCalledWith(mockKeypair.publicKey);
+    expect(balance).toBe(10);
+  });
+
+  test('should handle connection errors gracefully', async () => {
+    jest.spyOn(mockConnection, 'getBalance').mockRejectedValue(new Error('Connection error'));
+
+    await expect(getBalance(mockConnection, mockKeypair)).rejects.toThrow('Connection error');
+  });
+
+  test('should correctly transform lamports to SOL', async () => {
+    jest.spyOn(mockConnection, 'getBalance').mockResolvedValue(1000000000);
+
+    const balance = await getBalance(mockConnection, mockKeypair);
+    
+    expect(balance).toBe(1); // 1000000000 лампортов = 1 SOL
+  });
+});
